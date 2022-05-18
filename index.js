d7c3056bb679d9d24d89d33cf421b9ea969e5549
maddygoround <rathodm63@gmail.com>
2022-05-18 12:38:33 +0530

    changes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const core = require("@actions/core");
+const pip3 = require("pip3");
 // const github = require("@actions/github");
 
 try {
@@ -6,7 +7,16 @@ try {
   const nameToGreet = core.getInput("who-to-greet");
   console.log(`Hello ${nameToGreet}!`);
   const time = new Date().toTimeString();
-  core.setOutput("time", time);
+  pip3.install().then((success) => {
+    if (success) {
+      core.setOutput("time", time);
+      console.log("pip3 has been succesfully installed!");
+      pip3.exec("--version");
+    } else {
+      console.log("Installation of pip3 failed.");
+    }
+  });
+  
   // Get the JSON webhook payload for the event that triggered the workflow
 //   const payload = JSON.stringify(github.context.payload, undefined, 2);
 //   console.log(`The event payload: ${payload}`);
---
+++ src/setup-python.js
@@ -0,0 +1,68 @@
+const  core =  require('@actions/core');
+import * as finder from './find-python';
+import * as finderPyPy from './find-pypy';
+import * as path from 'path';
+import * as os from 'os';
+import {getCacheDistributor} from './cache-distributions/cache-factory';
+import {isCacheFeatureAvailable} from './utils';
+
+function isPyPyVersion(versionSpec) {
+  return versionSpec.startsWith('pypy-');
+}
+
+async function cacheDependencies(cache, pythonVersion) {
+  const cacheDependencyPath =
+    core.getInput('cache-dependency-path') || undefined;
+  const cacheDistributor = getCacheDistributor(
+    cache,
+    pythonVersion,
+    cacheDependencyPath
+  );
+  await cacheDistributor.restoreCache();
+}
+
+async function run() {
+  if (process.env.AGENT_TOOLSDIRECTORY?.trim()) {
+    core.debug(
+      `Python is expected to be installed into AGENT_TOOLSDIRECTORY=${process.env['AGENT_TOOLSDIRECTORY']}`
+    );
+    process.env['RUNNER_TOOL_CACHE'] = process.env['AGENT_TOOLSDIRECTORY'];
+  } else {
+    core.debug(
+      `Python is expected to be installed into RUNNER_TOOL_CACHE==${process.env['RUNNER_TOOL_CACHE']}`
+    );
+  }
+  try {
+    const version = core.getInput('python-version');
+    if (version) {
+      let pythonVersion;
+      const arch = core.getInput('architecture') || os.arch();
+      if (isPyPyVersion(version)) {
+        const installed = await finderPyPy.findPyPyVersion(version, arch);
+        pythonVersion = `${installed.resolvedPyPyVersion}-${installed.resolvedPythonVersion}`;
+        core.info(
+          `Successfully set up PyPy ${installed.resolvedPyPyVersion} with Python (${installed.resolvedPythonVersion})`
+        );
+      } else {
+        const installed = await finder.useCpythonVersion(version, arch);
+        pythonVersion = installed.version;
+        core.info(`Successfully set up ${installed.impl} (${pythonVersion})`);
+      }
+
+      const cache = core.getInput('cache');
+      if (cache && isCacheFeatureAvailable()) {
+        await cacheDependencies(cache, pythonVersion);
+      }
+    } else {
+      core.warning(
+        'The `python-version` input is not set.  The version of Python currently in `PATH` will be used.'
+      );
+    }
+    const matchersPath = path.join(__dirname, '../..', '.github');
+    core.info(`##[add-matcher]${path.join(matchersPath, 'python.json')}`);
+  } catch (err) {
+    core.setFailed((err as Error).message);
+  }
+}
+
+run();
