7d409a1c9f0dc0fbf00babcf301ad0f6d6e3e742
maddygoround <rathodm63@gmail.com>
2022-05-18 18:29:42 +0530

    Action,workflow and entrypoint updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/sh -l
 echo "Hello $1"
 echo "Mahendra Rathod"
-date
-ls
 whispers ./secrets-not-allowed >> output.txt
-echo "hello" >> output.txt
 filecontent=`cat output.txt`
-echo "::set-output name=time::$filecontent"
+echo "$filecontent"
+if [ -s output.txt ]; then
+  echo "Vulnerabilities found!"
+  exit 1
+else
+    rm -rf output.txt
+    echo "::set-output name=result::$filecontent"
+fi
