2dab8dd7aa3cac692a3ac4f67fca77ba59bffdcc
maddygoround <rathodm63@gmail.com>
2022-05-18 16:50:59 +0530

    entryfuke chnaged

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
 echo "Hello $1"
 whispers ./ >> output.txt
-filecontent=$(cat output.txt)
+filecontent=`cat output.txt`
+echo "$filecontent"
 echo "::set-output name=time::$filecontent"
