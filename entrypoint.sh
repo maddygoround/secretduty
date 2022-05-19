dd2b5eb62a41afa8012f8550bd9b853c06ed5e75
maddygoround <rathodm63@gmail.com>
2022-05-19 11:45:19 +0530

    changed in the way input is processed

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 echo "$1"
 t=$1
-t=$(sed -e 's/\[//g' -e 's/\]//g' -e 's/,$//' <<< $t)
+t=`sed -e 's/\[//g' -e 's/\]//g' -e 's/,$//' <<< $t`
 echo $t
 whispers ./ >> output.txt
 filecontent=`cat output.txt`
