43b6e3eec645ca1de5d28091a263fd33f025e875
maddygoround <rathodm63@gmail.com>
2022-05-19 12:01:45 +0530

    severity defined

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,18 @@
 echo "$1" >> input.txt
 t=`sed -e 's/\[//g' -e 's/\]//g' -e 's/,$//' < input.txt`
 echo $t
-whispers ./ >> output.txt
+
+if [ "${t}" ]; then
+  echo "execute with severity - $t"
+  whispers --severity $t ./ >> output.txt
+else
+  whispers ./ >> output.txt
+fi
+
 filecontent=`cat output.txt`
+
 echo "$filecontent"
+
 if [ -s output.txt ]; then
   echo "Vulnerabilities found!"
   exit 1
