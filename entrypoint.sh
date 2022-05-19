13f4813d2c829ba35056c1ce5f2693c06032be1f
maddygoround <rathodm63@gmail.com>
2022-05-19 12:47:33 +0530

    entrypoint file updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,15 @@
 #!/bin/sh -l
-echo "$1" >> input.txt
-t=`sed -e 's/\[//g' -e 's/\]//g' -e 's/,$//' < input.txt`
-echo $t
-whispers --severity "$t" ./ >> output.txt
-whispers ./ >> output1.txt
-cat output.txt
-cat output1.txt
+t=`sed -e 's/\[//g' -e 's/\]//g' -e 's/,$//' -e "s/['\"]//g" < $1`
+whispers --severity $t ./ >> output.txt
 filecontent=`cat output.txt`
 if [ -s output.txt ]; then
   echo "Vulnerabilities found!"
   echo "::set-output name=result::$filecontent"
+  # rm -rf input.txt
+  rm -rf output.txt
   exit 1
 else
+    # rm -rf input.txt
     rm -rf output.txt
     echo "::set-output name=result::$filecontent"
 fi
